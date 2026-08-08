53adf215d68ddbb0a0134d0235e221bb89fc771c
SyedShahidAhamed <syedshahidahamed786@gmail.com>
2026-08-08 11:59:24 +0530

    Secure database configuration with environment variables

--- src/main/java/com/shahid/shopsphere/controller/ProductController.java
+++ src/main/java/com/shahid/shopsphere/controller/ProductController.java
@@ -1,7 +1,5 @@
 package com.shahid.shopsphere.controller;
 
-
-
 import java.math.BigDecimal;
 
 import org.springframework.http.HttpStatus;
@@ -32,149 +30,109 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
 @SecurityRequirement(name = "Bearer Authentication")
-@Tag(
-    name="Products",
-    description="Product Management APIs"
-)
+@Tag(name = "Products", description = "Product Management APIs")
 public class ProductController {
-     private final ProductService productService;
+   private final ProductService productService;
 
-     @Operation(
-        summary="Create  Product",
-        description="Create A New Product."
-     )
-     @ApiResponses({
-        @ApiResponse(responseCode="201",description="Product Created"),
-        @ApiResponse(responseCode="404",description="Product Not Created"),
-        @ApiResponse(responseCode="401",description="unauthorized")
-    })
+   @Operation(summary = "Create  Product", description = "Create A New Product.")
+   @ApiResponses({
+         @ApiResponse(responseCode = "201", description = "Product Created"),
+         @ApiResponse(responseCode = "404", description = "Product Not Created"),
+         @ApiResponse(responseCode = "401", description = "unauthorized")
+   })
 
-    @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<ProductResponse> createProduct(
-        
-        @Valid @RequestBody ProductRequest request
-        )
-    {
-        ProductResponse productResponse = productService.createProduct(request);
+   @PostMapping
+   @PreAuthorize("hasRole('ADMIN')")
+   public ResponseEntity<ProductResponse> createProduct(
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
-    }
-   
+         @Valid @RequestBody ProductRequest request) {
+      ProductResponse productResponse = productService.createProduct(request);
 
-    @Operation(
-        summary = "Get All Products",
-        description = "Returns a paginated list of products with optional sorting and filtering."
-     )
-     @ApiResponses({
-        @ApiResponse(responseCode="200",description="Products Retrived Successfully"),
-        @ApiResponse(responseCode="401",description="unauthorized"),
-        @ApiResponse(responseCode="500",description="Internal Server Error")
-    })
-    @GetMapping  
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")                                       
-    public ResponseEntity<PageResponse<ProductResponse>> getAllProducts(
-     @Parameter(description="Page Number",example="0")
-    @RequestParam(defaultValue = "0")int page,
+      return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+   }
 
-    @Parameter(description="Page Size",example="10")
-    @RequestParam(defaultValue = "10") int size,
+   @Operation(summary = "Get All Products", description = "Returns a paginated list of products with optional sorting and filtering.")
+   @ApiResponses({
+         @ApiResponse(responseCode = "200", description = "Products Retrived Successfully"),
+         @ApiResponse(responseCode = "401", description = "unauthorized"),
+         @ApiResponse(responseCode = "500", description = "Internal Server Error")
+   })
+   @GetMapping
+   @PreAuthorize("hasAnyRole('USER','ADMIN')")
+   public ResponseEntity<PageResponse<ProductResponse>> getAllProducts(
+         @Parameter(description = "Page Number", example = "0") @RequestParam(defaultValue = "0") int page,
 
-    @Parameter(description="Sort Field",example="price")
-    @RequestParam(defaultValue = "price") String sortBy,
+         @Parameter(description = "Page Size", example = "10") @RequestParam(defaultValue = "10") int size,
 
-    @Parameter(description="Sort Direction",example="desc")
-    @RequestParam(defaultValue = "asc") String direction,
+         @Parameter(description = "Sort Field", example = "price") @RequestParam(defaultValue = "price") String sortBy,
 
-    @Parameter(description="Category Name",example="Mobiles")
-    @RequestParam(required =false) String category,
+         @Parameter(description = "Sort Direction", example = "desc") @RequestParam(defaultValue = "asc") String direction,
 
-    @Parameter(description="Brand Name",example="Apple")
-    @RequestParam(required=false) String brand,
+         @Parameter(description = "Category Name", example = "Mobiles") @RequestParam(required = false) String category,
 
-    @Parameter(description="Minimum Price",example="5000")
-    @RequestParam(required = false) BigDecimal minPrice,
+         @Parameter(description = "Brand Name", example = "Apple") @RequestParam(required = false) String brand,
 
-    @Parameter(description="Maximum Price",example="20,000")
-    @RequestParam(required = false) BigDecimal maxPrice,
+         @Parameter(description = "Minimum Price", example = "5000") @RequestParam(required = false) BigDecimal minPrice,
 
-    @Parameter(description="Search Keyword",example="iphone")
-    @RequestParam(required =false) String keyword)
-    {
-        PageResponse<ProductResponse> productResponses = productService.getAllProducts(page,size,sortBy,direction,category,brand,minPrice,maxPrice,keyword);
+         @Parameter(description = "Maximum Price", example = "20000") @RequestParam(required = false) BigDecimal maxPrice,
 
-        return ResponseEntity.ok(productResponses);
-    }
-     @Operation(
-        summary="Get Product By ID",
-        description="Fetches a product using its unique ID."
-     )
+         @Parameter(description = "Search Keyword", example = "iphone") @RequestParam(required = false) String keyword) {
+      PageResponse<ProductResponse> productResponses = productService.getAllProducts(page, size, sortBy, direction,
+            category, brand, minPrice, maxPrice, keyword);
 
-     @ApiResponses({
-        @ApiResponse(responseCode="200",description="Product Found"),
-        @ApiResponse(responseCode="404",description="Product Not Found"),
-        @ApiResponse(responseCode="401",description="unauthorized")
-    })
-    @GetMapping("/{id}")
-     @PreAuthorize("hasAnyRole('USER','ADMIN')")
-     public ResponseEntity<ProductResponse> getProductById
-     (
-        @Parameter(description="Product Id",example="1")
-        @PathVariable Long id
-     )
-     {
-     ProductResponse productResponse = productService.getProductById(id);
+      return ResponseEntity.ok(productResponses);
+   }
 
-     return ResponseEntity.status(HttpStatus.OK).body(productResponse);
+   @Operation(summary = "Get Product By ID", description = "Fetches a product using its unique ID.")
 
-     }
+   @ApiResponses({
+         @ApiResponse(responseCode = "200", description = "Product Found"),
+         @ApiResponse(responseCode = "404", description = "Product Not Found"),
+         @ApiResponse(responseCode = "401", description = "unauthorized")
+   })
+   @GetMapping("/{id}")
+   @PreAuthorize("hasAnyRole('USER','ADMIN')")
+   public ResponseEntity<ProductResponse> getProductById(
+         @Parameter(description = "Product Id", example = "1") @PathVariable Long id) {
+      ProductResponse productResponse = productService.getProductById(id);
 
-     @Operation(
-        summary="Update Product By ID",
-        description="Update an Existing Product and save in database."
-     )
-     @ApiResponses({
-        @ApiResponse(responseCode="20",description="Product Updated successfully"),
-        @ApiResponse(responseCode="404",description="Product Not Found"),
-        @ApiResponse(responseCode="401",description="unauthorized")
-    })
-    @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-     public ResponseEntity<ProductResponse> updateProduct(
-        @Parameter(description="Product Id",example="1")
-        @PathVariable Long id,
-        @Valid @RequestBody ProductRequest request){
+      return ResponseEntity.status(HttpStatus.OK).body(productResponse);
 
-    ProductResponse productResponse = productService.updateProduct(id, request);
+   }
 
-    return ResponseEntity.ok(productResponse);
-     }
-        
-     @Operation(
-        summary="Delete Product By ID",
-        description="Delete an Existing Product and remove from database."
-     )
-     @ApiResponses({
-        @ApiResponse(responseCode="204",description="Product Deleted"),
-        @ApiResponse(responseCode="404",description="Product Not Found"),
-        @ApiResponse(responseCode="401",description="unauthorized")
-    })
-        @DeleteMapping("/{id}")
-        @PreAuthorize("hasRole('ADMIN')")
-       public ResponseEntity<Void> deleteProduct
-       (
-        @Parameter(description="Product Id",example="1")
-        @PathVariable Long id
-        )
-       {
-            productService.deleteProduct(id);
-              return ResponseEntity.noContent().build();
-       }
-    
-    
+   @Operation(summary = "Update Product By ID", description = "Update an Existing Product and save in database.")
+   @ApiResponses({
+         @ApiResponse(responseCode = "20", description = "Product Updated successfully"),
+         @ApiResponse(responseCode = "404", description = "Product Not Found"),
+         @ApiResponse(responseCode = "401", description = "unauthorized")
+   })
+   @PutMapping("/{id}")
+   @PreAuthorize("hasRole('ADMIN')")
+   public ResponseEntity<ProductResponse> updateProduct(
+         @Parameter(description = "Product Id", example = "1") @PathVariable Long id,
+         @Valid @RequestBody ProductRequest request) {
+
+      ProductResponse productResponse = productService.updateProduct(id, request);
+
+      return ResponseEntity.ok(productResponse);
+   }
+
+   @Operation(summary = "Delete Product By ID", description = "Delete an Existing Product and remove from database.")
+   @ApiResponses({
+         @ApiResponse(responseCode = "204", description = "Product Deleted"),
+         @ApiResponse(responseCode = "404", description = "Product Not Found"),
+         @ApiResponse(responseCode = "401", description = "unauthorized")
+   })
+   @DeleteMapping("/{id}")
+   @PreAuthorize("hasRole('ADMIN')")
+   public ResponseEntity<Void> deleteProduct(
+         @Parameter(description = "Product Id", example = "1") @PathVariable Long id) {
+      productService.deleteProduct(id);
+      return ResponseEntity.noContent().build();
+   }
+
 }
